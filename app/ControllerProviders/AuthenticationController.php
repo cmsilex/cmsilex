b8e56082dc01d3c2ace3fd901adf6b01caf10f1e
Leigh Murray <leighmurray@2wattend.com>
2016-06-20 02:11:05 +1000

    neated it up

--- app/ControllerProviders/AuthenticationController.php
+++ app/ControllerProviders/AuthenticationController.php
@@ -17,73 +17,77 @@ class AuthenticationController implements ControllerProviderInterface
     {
         $controllers = $app['controllers_factory'];
 
-        $controllers->match('/login', function (Application $app, Request $request) {
-            $form = $app['form.factory']->createNamedBuilder(null)
-                ->add('_username')
-                ->add('_password')
-                ->add('submit', SubmitType::class)
-                ->setAction($app->url('login_check'))
-                ->getForm()
-            ;
-
-            $form->handleRequest($request);
-
-            $form = $form->createView();
-
-            return $app->render('authentication/login.html.twig', [
-                'form' => $form,
-                'error'         => $app['security.last_error']($request),
-                'last_username' => $app['session']->get('_security.last_username'),
-            ]);
-        })
-        ->method("POST|GET")
-        ->bind('login')
+        $controllers->match('/login', 'CMSilex\ControllerProviders\AuthenticationController::loginAction')
+            ->method("POST|GET")
+            ->bind('login')
         ;
 
         if ($app['config']['register'] == true) {
-            $controllers->match('/register', function (Application $app, Request $request) {
-                $builder = $app->form();
-                $builder
-                    ->add('email', EmailType::class)
-                    ->add('password', RepeatedType::class, [
-                        'type' => PasswordType::class,
-                        'first_options' => [
-                            'label' => 'Password'
-                        ],
-                        'second_options' => [
-                            'label' => 'Repeat Password'
-                        ]
-                    ])
-                    ->add('register', SubmitType::class)
-                ;
-                $form = $builder->getForm();
-
-                $form->handleRequest($request);
-                if ($form->isSubmitted() && $form->isValid())
-                {
-                    $userInfo = $form->getData();
-
-                    $newUser = new User();
-                    $password = $app->encodePassword($newUser, $userInfo['password']);
-                    $newUser->setUsername($userInfo['email']);
-                    $newUser->setPassword($password);
-                    $newUser->setEnabled(true);
-                    $newUser->setAccountNonExpired(true);
-                    $newUser->setAccountNonLocked(true);
-                    $newUser->setCredentialsNonExpired(true);
-                    $newUser->setRoles(['ROLE_USER']);
-                    $app['em']->persist($newUser);
-                    $app['em']->flush();
-
-                    return $app->redirect($app->url('login'));
-                }
-
-                return $app->render('authentication/register.html.twig', [
-                    'form' => $form->createView()
-                ]);
-            });
+            $controllers->match('/register', 'CMSilex\ControllerProviders\AuthenticationController::loginAction');
         }
 
         return $controllers;
+    }
+
+    public function loginAction (Application $app, Request $request) {
+        $form = $app['form.factory']->createNamedBuilder(null)
+            ->add('_username')
+            ->add('_password')
+            ->add('submit', SubmitType::class)
+            ->setAction($app->url('login_check'))
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        $form = $form->createView();
+
+        return $app->render('authentication/login.html.twig', [
+            'form'          => $form,
+            'error'         => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
+        ]);
+    }
+
+    public function registerAction (Application $app, Request $request) {
+        $builder = $app->form();
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Password'
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password'
+                ]
+            ])
+            ->add('register', SubmitType::class)
+        ;
+        $form = $builder->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $userInfo = $form->getData();
+
+            $newUser = new User();
+            $password = $app->encodePassword($newUser, $userInfo['password']);
+            $newUser->setUsername($userInfo['email']);
+            $newUser->setPassword($password);
+            $newUser->setEnabled(true);
+            $newUser->setAccountNonExpired(true);
+            $newUser->setAccountNonLocked(true);
+            $newUser->setCredentialsNonExpired(true);
+            $newUser->setRoles(['ROLE_USER']);
+            $app['em']->persist($newUser);
+            $app['em']->flush();
+
+            return $app->redirect($app->url('login'));
+        }
+
+        return $app->render('authentication/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
